435875b696813d694ba98553197a07ad47a36fa2
Jonathan Karr <jonrkarr@gmail.com>
2017-11-29 19:25:13 -0500

    making imports work when packages not insalled

--- conv_opt/solver/__init__.py
+++ conv_opt/solver/__init__.py
@@ -1,11 +1,39 @@
-from .cplex import CplexModel
+""" Open-source, easily installable solvers """
 from .cvxopt import CvxoptModel
 from .cvxpy import CvxpyModel
-from .cylp import CylpModel
 from .glpk import GlpkModel
-from .gurobi import GurobiModel
-from .mosek import MosekModel
 from .optlang import OptlangModel
 from .quadprog import QuadprogModel
 from .scipy import ScipyModel
-from .xpress import XpressModel
+
+""" Optional solvers """
+
+# IBM CPLEX
+try:
+    from .cplex import CplexModel
+except ImportError:
+    pass
+
+# CyLP/CBC
+try:
+    from .cylp import CylpModel
+except ImportError:
+    pass
+
+# Gurobi
+try:
+    from .gurobi import GurobiModel
+except ImportError:
+    pass
+
+# MOSEK
+try:
+    from .mosek import MosekModel
+except ImportError:
+    pass
+
+# FICO XPRESS
+try:
+    from .xpress import XpressModel
+except ImportError:
+    pass
