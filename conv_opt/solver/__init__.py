fe536898cf103157a8cd4db4a03d5b7c08d0cd29
Jonathan Karr <jonrkarr@gmail.com>
2017-12-18 15:35:38 -0500

    marking lines that shouldn't be covered by tests

--- conv_opt/solver/__init__.py
+++ conv_opt/solver/__init__.py
@@ -12,28 +12,28 @@ from .scipy import ScipyModel
 try:
     from .cplex import CplexModel
 except ImportError:
-    pass
+    pass  # pragma: no cover
 
 # CyLP/CBC
 try:
     from .cylp import CylpModel
 except ImportError:
-    pass
+    pass  # pragma: no cover
 
 # Gurobi
 try:
     from .gurobi import GurobiModel
 except ImportError:
-    pass
+    pass  # pragma: no cover
 
 # MOSEK
 try:
     from .mosek import MosekModel
 except ImportError:
-    pass
+    pass  # pragma: no cover
 
 # FICO XPRESS
 try:
     from .xpress import XpressModel
 except ImportError:
-    pass
+    pass  # pragma: no cover
