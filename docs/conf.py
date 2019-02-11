cece4957ce51ead3962eeec3d66bb957d907aa1f
Jonathan Karr <jonrkarr@gmail.com>
2019-02-11 12:34:29 -0500

    fixing docs searching and pretty printing results

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ extensions = [
     'sphinxcontrib.bibtex',
     'sphinxcontrib.googleanalytics',
     'sphinxcontrib.spelling',
+    'sphinxprettysearchresults',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
