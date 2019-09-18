078a079ef7955bbfa5ad92611699d87841221f73
Jonathan Karr <jonrkarr@gmail.com>
2019-09-18 16:40:49 -0400

    changing default contact address

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     url="https://github.com/KarrLab/" + name,
     download_url='https://github.com/KarrLab/' + name,
     author="Karr Lab",
-    author_email="karr@mssm.com",
+    author_email="info@karrlab.org",
     license="MIT",
     keywords='convex optimization, linear programming, quadratic programming',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
