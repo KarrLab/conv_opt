d338bd33d02e3ecd3e47dc5ea4393a68f8c9e56f
Jonathan Karr <jonrkarr@gmail.com>
2020-02-24 13:54:04 -0500

    renaming version file for consistency with semantic versioning

--- conv_opt/__init__.py
+++ conv_opt/__init__.py
@@ -1,7 +1,6 @@
 import pkg_resources
 
-with open(pkg_resources.resource_filename('conv_opt', 'VERSION'), 'r') as file:
-    __version__ = file.read().strip()
+from ._version import __version__
 # :obj:`str`: version
 
 from .core import (ExportFormat, ModelType, ObjectiveDirection, Presolve,
